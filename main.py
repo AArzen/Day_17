22641b4c2248788be6984a4a7d3de8a45f7b6e93
A Arzen <anze.ar@gmail.com>
2023-07-02 23:08:27 +0200

    Secret number from SQL

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import hashlib
 import uuid
-from flask import Flask, render_template, request, make_response, url_for, redirect
+from flask import Flask, render_template, request, make_response, redirect, url_for
 import random
 from sqla_wrapper import SQLAlchemy
 
@@ -31,39 +31,45 @@ def get_user():
 
 @app.route("/", methods=["GET"])
 def secret_number():
-    answer = request.cookies.get("answer")
-    if get_user() is not None:
+    # answer = request.cookies.get("answer")
+    user = get_user()
+
+    if user is None:
+        return render_template("secretNumber.html")
+    else:
         user = get_user()
-        user = user.email
-    else:
-        user = ''
+        if user.secret_number == 0:
 
-    if not answer or answer == 0:
-        answer = random.randint(1, 30)
-        response = make_response(render_template("secretNumber.html", answer=answer, user=user))
-        response.set_cookie("answer", str(answer))
-        return response
+            user.secret_number = random.randint(1, 30)
+            user.save()
+            return render_template("secretNumber.html", answer=user.secret_number)
 
-    else:
-        answer = answer
+        else:
 
-    return render_template("secretNumber.html", answer=answer)
+            return render_template("secretNumber.html", answer=user.secret_number)
 
 
 @app.route("/answer", methods=["POST"])
 def check_answer():
-    answer = int(request.cookies.get("answer"))
-    secret = int(request.form.get("secret_number"))
-
-    if answer == secret:
-
-        dialog = f"{str(secret)}: CORRECT"
-
-        return render_template("checkAnswer.html", answer=answer, result=dialog)
+    user = get_user()
+    if user is None:
+        return render_template('message.html', type='danger', message="Please login", redirect=True)
     else:
+        answer = int(user.secret_number)
+        # answer = int(request.cookies.get("answer"))
+        secret = int(request.form.get("secret_number"))
 
-        dialog = f"{str(secret)}: INCORRECT"
-        return render_template("checkAnswer.html", answer=answer, result=dialog)
+        if answer == secret:
+
+            dialog = f"{str(secret)}: CORRECT"
+            user.secret_number = random.randint(1, 30)
+            user.save()
+
+            return render_template("checkAnswer.html", answer=answer, result=dialog)
+        else:
+
+            dialog = f"{str(secret)}: INCORRECT"
+            return render_template("checkAnswer.html", answer=answer, result=dialog)
 
 
 @app.route("/users")
